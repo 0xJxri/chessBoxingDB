b81aec60b89709842f761f849da2fd7d14f21a7e
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2024-04-05 01:24:03 +0200

    fetch data to /results

--- Frontend/pages/results/index.vue
+++ Frontend/pages/results/index.vue
@@ -66,6 +66,9 @@
             <div class="w-4 h-4 bg-white rounded-full"></div>
             <span>Fighter White</span>
           </TableHead>
+          <TableHead class="px-0">
+            VS
+          </TableHead>
           <TableHead>
             <div class="flex items-center justify-start gap-2">
               <span>Fighter Black</span>
@@ -81,14 +84,41 @@
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow v-for="item in data.payload" :key="item._id">
-          <TableCell class="text-right">{{ item.fighterWhite }}</TableCell>
-          <TableCell>{{ item.fighterBlack }}</TableCell>
+        <TableRow v-for="item in results.payload" :key="item._id">
+          <TableCell>
+            <div class="flex justify-end items-center gap-3">
+              <span>{{ item.fighterWhite }}</span>
+              <img
+                class="w-7 h-7 rounded-full"
+                :src="
+                  item.urlImgWhite
+                    ? item.urlImgWhite + '_thumb.jpg'
+                    : 'https://www.chessboxing.info/images/avatar_unknown.jpg'
+                "
+              />
+            </div>
+          </TableCell>
+          <TableCell class="px-0">
+            vs
+          </TableCell>
+          <TableCell>
+            <div class="flex justify-start items-center gap-3">
+              <img
+                class="w-7 h-7 rounded-full"
+                :src="
+                  item.urlImgBlack
+                    ? item.urlImgBlack + '_thumb.jpg'
+                    : 'https://www.chessboxing.info/images/avatar_unknown.jpg'
+                "
+              />
+              <span>{{ item.fighterBlack }}</span>
+            </div>
+          </TableCell>
           <TableCell>{{ item.result }}</TableCell>
           <TableCell>{{ item.event }}</TableCell>
           <TableCell>{{ item.data }}</TableCell>
           <TableCell>
-            <ArrowRightToLine class="ml-auto"/>
+            <ArrowRightToLine class="ml-auto" />
           </TableCell>
         </TableRow>
       </TableBody>
@@ -96,7 +126,11 @@
 
     <!-- grid layout -->
     <div v-show="isGridLayoutSelected" class="grid grid-cols-3 gap-4">
-      <Card v-for="item in data.payload" :key="item._id" class="w-full bg-background">
+      <Card
+        v-for="item in results.payload"
+        :key="item._id"
+        class="w-full bg-background"
+      >
         <CardHeader class="flex items-center">
           <p class="font-bold">{{ item.event }}</p>
           <p class="text-xs text-zinc-400 italic">{{ item.data }}</p>
@@ -104,7 +138,11 @@
         <CardContent>
           <div class="relative w-full flex justify-center">
             <p
-              class="absolute z-10 top-6 opacity-60 font-bold font-blaka text-7xl"
+              class="absolute z-10 top-16 opacity-100 text-black font-bold font-blaka text-6xl"
+              style="
+                text-shadow: -1px -1px 0 white, 1px -1px 0 white,
+                  -1px 1px 0 white, 1px 1px 0 white;
+              "
             >
               VS
             </p>
@@ -113,7 +151,11 @@
             <div class="relative rounded-t-lg overflow-clip rounded-es-3xl">
               <img
                 class="border-t-8 border-white"
-                :src="item.whitePfp"
+                :src="
+                  item.urlImgWhite
+                    ? item.urlImgWhite + '_big.jpg'
+                    : 'https://www.chessboxing.info/images/profile_unknown.jpg'
+                "
                 alt="left-fighter"
               />
               <p
@@ -129,7 +171,11 @@
             <div class="relative rounded-t-lg overflow-clip rounded-ee-3xl">
               <img
                 class="border-t-8 border-black"
-                :src="item.blackPfp"
+                :src="
+                  item.urlImgBlack
+                    ? item.urlImgBlack + '_big.jpg'
+                    : 'https://www.chessboxing.info/images/profile_unknown.jpg'
+                "
                 alt="left-fighter"
               />
               <p
@@ -153,120 +199,5 @@
 import { Search, Rows3, Grid3X3, ArrowRightToLine } from "lucide-vue-next";
 
 const isGridLayoutSelected = ref(false);
-
-const data = {
-  status: "success",
-  message: "Results found",
-  code: 200,
-  payload: [
-    {
-      _id: "6606c6e0902c732ad64b1d09",
-      fighterWhite: "Timur Iyubinskii",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_310321113920_001_010521232531_big.jpg",
-      fighterBlack: "Gianpiero Sportelli",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_020521124048_001_020521130510_big.jpg",
-      result: "W-L",
-      event: "WCBO - World Chessboxing Championships 2019",
-      data: "15 December 2019",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c86",
-      fighterWhite: "Sergei 'Giraffe' Fedorenko",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_211122230716_001_220923232241_big.jpg",
-      fighterBlack: "Arnold 'Confectioner' Baklizky",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_251221230542_001_250222200817_mid.jpg",
-      result: "L-W",
-      event: "FSB - International Chessboxing Grand Prix 85kg",
-      data: "08 April 2023",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c54",
-      fighterWhite: "Remmo Ali",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_021123232118_001_021123232125_big.jpg",
-      fighterBlack: "Sehirli Berk",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_251222231302_001_281123235035_big.jpg",
-      result: "L-W",
-      event: "WCBO - World Chessboxing Championships 2023",
-      data: "28 October 2023",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1cf6",
-      fighterWhite: "Richard 'The Razor' Frazer",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_021123220807_001_021123220835_big.jpg",
-      fighterBlack: "Dan 'The Taxman' Manfold",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_211122232102_002_021123221207_big.jpg",
-      result: "L-W",
-      event: "LCB - 36 Clash of Kings 2020",
-      data: "26 July 2020",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c98",
-      fighterWhite: "Utkirbek Ergashev",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_211122230716_001_220923232241_big.jpg",
-      fighterBlack: "Aleksandr 'E4' Smirnov",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_251221230542_001_250222200817_mid.jpg",
-      result: "L-W",
-      event: "FSB - International Chessboxing Grand Prix 75kg",
-      data: "14 January 2023",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c74",
-      fighterWhite: "Alexander Kuchkin",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_021123232118_001_021123232125_big.jpg",
-      fighterBlack: "Valery Volozhenin",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_251222231302_001_281123235035_big.jpg",
-      result: "W-L",
-      event: "FSB - International Chessboxing Grand Prix 85kg",
-      data: "09 September 2023",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1d40",
-      fighterWhite: "Lars 'Lazarus' Bjorknas",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_310321113920_001_010521232531_big.jpg",
-      fighterBlack: "Cameron 'The Hurt Locker' Little",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_020521124048_001_020521130510_big.jpg",
-      result: "L-W",
-      event: "LCB - Seasons Beatings 2018",
-      data: "08 December 2018",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c35",
-      fighterWhite: "Arminius Rolle",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_021123220807_001_021123220835_big.jpg",
-      fighterBlack: "Stefano Tudoran",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_211122232102_002_021123221207_big.jpg",
-      result: "L-W",
-      event: "CBCB - Chessboxing Fight Night",
-      data: "23 November 2023",
-    },
-    {
-      _id: "6606c6e0902c732ad64b1c94",
-      fighterWhite: "Dogukan 'Big Brother' Cinar",
-      whitePfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_211122230716_001_220923232241_big.jpg",
-      fighterBlack: "Hasan Celik",
-      blackPfp:
-        "https://www.chessboxing.info/images/uploads/fighters/f_251221230542_001_250222200817_mid.jpg",
-      result: "W-L",
-      event: "CBP - Intellectual Fight Club 3",
-      data: "03 February 2023",
-    },
-  ],
-};
+const { data: results } = await useFetch("http://localhost:8000/results");
 </script>
