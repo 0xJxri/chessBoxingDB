3160d9496090df380f5c45cb27298a6721e74204
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2024-04-18 02:21:05 +0200

    chessboard

--- Frontend/pages/results/[date].vue
+++ Frontend/pages/results/[date].vue
@@ -29,9 +29,13 @@
       class="mx-auto flex flex-col items-center w-full max-w-lg font-bold text-3xl mb-6"
     >
       <span class="z-10 w-full relative flex items-center gap-3">
-        <span>
-          {{ fightDetails.payload[0].fighterBlack.Name }}
-        </span>
+        <nuxt-link
+          :to="'/fighters/' + fightDetails.payload[0].fighterBlack.Name"
+        >
+          <span class="underline hover:text-primary">
+            {{ fightDetails.payload[0].fighterBlack.Name }}
+          </span>
+        </nuxt-link>
         <img
           v-if="fightDetails.payload[0].nationalityBlack"
           class="h-5"
@@ -55,13 +59,20 @@
           :src="`https://flagcdn.com/${fightDetails.payload[0].nationalityWhite}.svg`"
           alt="flag"
         />
-        <span>
-          {{ fightDetails.payload[0].fighterWhite.Name }}
-        </span>
+        <nuxt-link
+          :to="'/fighters/' + fightDetails.payload[0].fighterWhite.Name"
+        >
+          <span class="underline hover:text-primary">
+            {{ fightDetails.payload[0].fighterWhite.Name }}
+          </span>
+        </nuxt-link>
       </span>
     </div>
 
-    <Carousel v-if="fightDetails.payload[0].gallery" class="relative w-full max-w-xl mx-auto mb-2">
+    <Carousel
+      v-if="fightDetails.payload[0].gallery"
+      class="relative w-full max-w-xl mx-auto mb-2"
+    >
       <CarouselContent>
         <CarouselItem
           v-for="(item, index) in fightDetails.payload[0].gallery"
@@ -80,7 +91,7 @@
       <CarouselNext />
     </Carousel>
 
-    <h3 class="text-center text-zinc-300">
+    <h3 class="text-center text-zinc-300 mb-24">
       <span>
         {{ fightDetails.payload[0].eventName }}
       </span>
@@ -94,13 +105,105 @@
       </span>
     </h3>
 
+    <div v-if="fightDetails.payload[0].chessMoves" class="grid grid-cols-3">
+      <div class="flex col-span-2 justify-center items-center gap-4 mb-12">
+        <ChevronLeft class="h-12 w-12 cursor-pointer" @click="previousMove()" />
+        <div class="w-2/3 flex flex-col items-center gap-2">
+          <div ref="myBoard" class="w-full"/>
+          <p class="text-xl">
+            {{ selectedChessMove }} /
+            {{ fightDetails.payload[0].chessMoves[0].length }}
+          </p>
+        </div>
+        <ChevronRight class="h-12 w-12 cursor-pointer" @click="nextMove()" />
+      </div>
+      <div class="col-span-1 flex items-center flex-wrap">
+        <p
+          v-for="(move, index) in fightDetails.payload[0].chessMoves[0]"
+          class="p-2 gap-2 mb-2 mr-2 rounded-sm"
+          :class="selectedChessMove === index+1 ? 'bg-primary' : 'bg-muted'"
+        >
+          <span>{{ index }}</span> {{ move }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-const route = useRoute();
+import {
+  Chessboard,
+  FEN,
+} from "~/node_modules/cm-chessboard/src/Chessboard.js";
+import { Chess } from "chess.js";
+import { ChevronRight, ChevronLeft } from "lucide-vue-next";
 
+const route = useRoute();
 const { data: fightDetails } = await useFetch(
   `http://localhost:8000/fightdetails?date=${route.params.date}&eventName=${route.query.eventName}&fighterWhite=${route.query.fighterWhite}&fighterBlack=${route.query.fighterBlack}`
 );
+
+const myBoard = ref();
+const selectedChessMove = ref(0);
+
+// const fenMoves = movesToFen(removeMoveNumbers(fightDetails.value.payload[0].chessMoves[0]));
+// console.log(fenMoves.value);
+
+function nextMove() {
+  if (
+    selectedChessMove.value < fightDetails.value.payload[0].chessMoves[0].length
+  ) {
+    selectedChessMove.value++;
+  } else {
+    selectedChessMove.value = 0;
+  }
+}
+
+function previousMove() {
+  if (selectedChessMove.value > 0) selectedChessMove.value--;
+  else
+    selectedChessMove.value =
+      fightDetails.value.payload[0].chessMoves[0].length;
+}
+
+function removeMoveNumbers(chessMoves) {
+    const regex = /^\d+\./; 
+    for (let i = 0; i < chessMoves.length; i++) {
+        chessMoves[i] = chessMoves[i].replace(regex, '');
+    }
+    return chessMoves;
+}
+
+function movesToFen(moves) {
+  const chess = new Chess();
+  const fenList = [];
+
+  for (let i = 0; i < moves.length; i += 2) {
+    const moveNumber = Math.floor(i / 2) + 1;
+    const whiteMove = moves[i];
+    const blackMove = moves[i + 1];
+
+    chess.move(whiteMove);
+    fenList.push({ moveNumber, fen: chess.fen() });
+
+    chess.move(blackMove);
+    fenList.push({ moveNumber, fen: chess.fen() });
+
+    console.log(fenList);
+  }
+
+  return fenList;
+}
+
+
+onMounted(() => {
+  const board = new Chessboard(myBoard.value, {
+    position: FEN.start,
+    assetsUrl: "/",
+  });
+});
 </script>
+
+<style>
+@import "~/node_modules/cm-chessboard/assets/chessboard.css";
+</style>
